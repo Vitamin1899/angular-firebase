d2deb95d94ffe724277208743cdda1fd55399962
Vitamin1899 <vitalimarinich@gmail.com>
2015-10-03 20:20:37 +0300

    initial commit; add auth

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 'use strict';
-
-// Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.home' // подключаем новый модуль
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+        // назначаем представление по умолчанию
+        $routeProvider.otherwise({
+            redirectTo: '/home'
+        });
+    }]);
