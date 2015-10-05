05a50ca3b8b70f90870158b88e9e47ba1abf0cdc
Vitamin1899 <vitalimarinich@gmail.com>
2015-10-05 16:21:53 +0300

    connection service firebase

--- app/home/home.js
+++ app/home/home.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute','firebase'])
     // Определение маршрута
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/home', {
@@ -8,5 +8,24 @@ angular.module('myApp.home', ['ngRoute'])
         });
     }])
     // контроллер home
-    .controller('HomeCtrl', [function() {
+    .controller('HomeCtrl',
+    ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+        $scope.SignIn = function(event) {
+            event.preventDefault(); // предотвращаем перезагрузку страницы
+            var username = $scope.user.email;
+            var password = $scope.user.password;
+            loginObj.$login('password', {
+                email: vitalimarinich@gmail.com,
+                password: marinich1986
+            })
+                .then(function(user) {
+                    // колбэк запустится при успешной аутентификации аутентификацииSuccess callback
+                    console.log('Authentication successful');
+                }, function(error) {
+                    console.log('Authentication failure');
+                });
+        // логика авторизации
+        }
+        var firebaseObj = new Firebase("https://blinding-torch-7780.firebaseapp.com");
+        var loginObj = $firebaseSimpleLogin(firebaseObj);
     }]);
