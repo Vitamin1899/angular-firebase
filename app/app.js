9cdc8f5aee15a114594b36ff54dc16a0ea6afdc9
Vitamin1899 <vitalimarinich@gmail.com>
2015-10-06 16:29:17 +0300

    create page register (route, validate, change form sign in)

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 'use strict';
+
 angular.module('myApp', [
     'ngRoute',
     'myApp.home' // подключаем новый модуль
+    'myApp.register'  // Newly added register route
 ]).
-    config(['$routeProvider', function($routeProvider) {
-        // назначаем представление по умолчанию
-        $routeProvider.otherwise({
-            redirectTo: '/home'
-        });
-    }]);
+config(['$routeProvider', function($routeProvider) {
+    // назначаем представление по умолчанию
+    $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
+}]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,23 +1,28 @@
 'use strict';
+
 angular.module('myApp.home', ['ngRoute','firebase'])
     // Определение маршрута
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/home', {
-            templateUrl: 'home/home.html',
-            controller: 'HomeCtrl'
-        });
-    }])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'home/home.html',
+        controller: 'HomeCtrl'
+    });
+}])
+
     // контроллер home
-    .controller('HomeCtrl',
-    ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
-        $scope.SignIn = function(event) {
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+    var firebaseObj = new Firebase("https://blinding-torch-7780.firebaseio.com");
+    var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+    $scope.user = {};
+    $scope.SignIn = function(event) {
             event.preventDefault(); // предотвращаем перезагрузку страницы
             var username = $scope.user.email;
             var password = $scope.user.password;
             loginObj.$login('password', {
-                email: vitalimarinich@gmail.com,
-                password: marinich1986
-            })
+                        email: username,
+                        password: password
+                    })
                 .then(function(user) {
                     // колбэк запустится при успешной аутентификации аутентификацииSuccess callback
                     console.log('Authentication successful');
@@ -26,6 +31,4 @@ angular.module('myApp.home', ['ngRoute','firebase'])
                 });
         // логика авторизации
         }
-        var firebaseObj = new Firebase("https://blinding-torch-7780.firebaseapp.com");
-        var loginObj = $firebaseSimpleLogin(firebaseObj);
-    }]);
+}]);
