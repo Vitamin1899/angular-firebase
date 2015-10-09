a7235c0e9e0b7b3287209ad1c857c426699e12de
Vitamin1899 <vitalimarinich@gmail.com>
2015-10-09 13:04:00 +0300

    some changes for easy work with app

--- app/addPost/addPost.js
+++ app/addPost/addPost.js
@@ -9,18 +9,20 @@ angular.module('myApp.addPost', ['ngRoute'])
     });
 }])
 
-.controller('AddPostCtrl', ['$scope','$firebase',function($scope,$firebase) {
+.controller('AddPostCtrl', ['$scope','$firebase','$location,','CommonProp',function($scope,$firebase,$location,CommonProp) {
     $scope.AddPost = function() {
       var title = $scope.article.title;
       var post = $scope.article.post;
 
-      var firebaseObj = new Firebase("https://blistering-heat-2473.firebaseio.com");
+      var firebaseObj = new Firebase("https://blinding-torch-7780.firebaseio.com/Articles");
       var fb = $firebase(firebaseObj);
 
-      fb.$push({title: title,post: post}).then(function(ref) {
+      fb.$push({title: title,post: post,emailId: CommonProp.getUser()}).then(function(ref) {
           console.log(ref);
+          $location.path('/welcome');
       }, function(error) {
           console.log("Error:", error);
       });
+
     }
 }]);
--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -9,6 +9,10 @@ angular.module('myApp.welcome', ['ngRoute'])
     });
 }])
 
-.controller('WelcomeCtrl', ['$scope', function($scope) {
+.controller('WelcomeCtrl', ['$scope','$firebase','CommonProp', function($scope,$firebase,CommonProp) {
     $scope.username = CommonProp.getUser();
+    var firebaseObj = new Firebase("https://blinding-torch-7780.firebaseio.com/Articles");
+
+    var sync = $firebase(firebaseObj);
+    $scope.articles = sync.$asArray();
 }]);
